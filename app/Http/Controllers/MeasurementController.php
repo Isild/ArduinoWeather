2574c192f0224c57bf9059ba7fcf7cf613aa69bc
Isild <pimok96@gmail.com>
2019-12-06 23:17:21 +0100

    make average and take n points

--- app/Http/Controllers/MeasurementController.php
+++ app/Http/Controllers/MeasurementController.php
@@ -43,7 +43,7 @@ class MeasurementController extends Controller
         $dateTimeStart = $yearStart . "-" . $monthStart . "-" . $dayStart . " " . $hourStart . ":" . $minutesStart . ":" . $secondsStart;
         $dateTimeEnd = $yearEnd . "-" . $monthEnd . "-" . $dayEnd . " " . $hourEnd . ":" . $minutesEnd . ":" . $secondsEnd;
 
-        return DB::select('
+        $dataMearsurment = DB::select('
             SELECT id AS number, date, temperature, air_pressure, air_humidity, rainfall, soil_moisture
             FROM measurement
             WHERE date > :dateTimeStart
@@ -52,6 +52,49 @@ class MeasurementController extends Controller
             'dateTimeStart'=>$dateTimeStart,
             'dateTimeEnd'=>$dateTimeEnd]
         );
+
+        //### to zamienia arraya JSONów na normalnego arraya do edycji
+        $dataToReturn = json_decode(json_encode($dataMearsurment), true);/*/
+        //$dataToReturn = json_encode($dataMearsurment);//*/
+        $temp = array();
+        $lengthArr = count($dataMearsurment);
+        $promil = round($lengthArr/1000);
+        if($lengthArr > 1000){
+            //początek
+            array_push($temp, [
+                'number'=>$dataToReturn[0]['number'],
+                'temperature'=>round(($dataToReturn[0]['temperature']+$dataToReturn[1]['temperature'])/2, 2),
+                'date'=>$dataToReturn[0]['date'],
+                'air_pressure'=>round(($dataToReturn[0]['air_pressure']+$dataToReturn[1]['air_pressure'])/2, 2),
+                'air_humidity'=>round(($dataToReturn[0]['air_humidity']+$dataToReturn[1]['air_humidity'])/2, 2),
+                'rainfall'=>round(($dataToReturn[0]['rainfall']+$dataToReturn[1]['rainfall'])/2, 2),
+                'soil_moisture'=>round(($dataToReturn[0]['soil_moisture']+$dataToReturn[1]['soil_moisture'])/2, 2),
+            ]);
+            //środkowe wartości
+            for($i=1; $i<$lengthArr-1; $i+=$promil){
+                array_push($temp, [
+                    'number'=>$dataToReturn[$i]['number'],
+                    'temperature'=>round(($dataToReturn[$i-1]['temperature']+$dataToReturn[$i]['temperature']+$dataToReturn[$i+1]['temperature'])/3, 2),
+                    'date'=>$dataToReturn[$i]['date'],
+                    'air_pressure'=>round(($dataToReturn[$i-1]['air_pressure']+$dataToReturn[$i]['air_pressure']+$dataToReturn[$i+1]['air_pressure'])/3, 2),
+                    'air_humidity'=>round(($dataToReturn[$i-1]['air_humidity']+$dataToReturn[$i]['air_humidity']+$dataToReturn[$i+1]['air_humidity'])/3, 2),
+                    'rainfall'=>round(($dataToReturn[$i-1]['rainfall']+$dataToReturn[$i]['rainfall']+$dataToReturn[$i+1]['rainfall'])/3, 2),
+                    'soil_moisture'=>round(($dataToReturn[$i-1]['soil_moisture']+$dataToReturn[$i]['soil_moisture']+$dataToReturn[$i+1]['soil_moisture'])/3, 2)
+                ]);
+            }
+            //koniec
+            array_push($temp, [
+                'number'=>$dataToReturn[$lengthArr-1]['number'],
+                'temperature'=>round(($dataToReturn[$lengthArr-2]['temperature']+$dataToReturn[$lengthArr-1]['temperature'])/2,2),
+                'date'=>$dataToReturn[$lengthArr-1]['date'],
+                'air_pressure'=>round(($dataToReturn[$lengthArr-2]['air_pressure']+$dataToReturn[$lengthArr-1]['air_pressure'])/2,2),
+                'air_humidity'=>round(($dataToReturn[$lengthArr-2]['air_humidity']+$dataToReturn[$lengthArr-1]['air_humidity'])/2,2),
+                'rainfall'=>round(($dataToReturn[$lengthArr-2]['rainfall']+$dataToReturn[$lengthArr-1]['rainfall'])/2,2),
+                'soil_moisture'=>round(($dataToReturn[$lengthArr-2]['soil_moisture']+$dataToReturn[$lengthArr-1]['soil_moisture'])/2,2)
+            ]);
+        }
+        //return json_encode($temp);
+        return $temp;
     }
 
     /**
